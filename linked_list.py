0f271700aa0855055c063cdcaf7d32dd2b022d9c
Benjamin Stoll <bstoll291997@gmail.com>
2025-03-25 11:19:56 -0500

    Initial Commit.

---
+++ linked_list.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, root: Node):
+        self.root = root
+
+
+    def linked_list_append(self, data) -> None:
+        current = self.root
+
+        while current.next != None:
+            current = current.next
+
+        current.next = Node(data)
+
+
+    def traverse_list(self) -> str:
+        traversal_string = ''
+        current = self.root
+
+        while current != None:
+            if current.next == None:
+                traversal_string += f'{str(current.data)}'
+            else:
+                traversal_string += f'{str(current.data)} -> '
+
+            current = current.next
+
+        return traversal_string
+    
+
+if __name__ == '__main__':
+    my_list = LinkedList(Node(1))
+
+    my_list.linked_list_append(2)
+    my_list.linked_list_append(3)
+    my_list.linked_list_append(4)
+    my_list.linked_list_append(5)
+    my_list.linked_list_append(6)
+
+    print(my_list.traverse_list())
+            
